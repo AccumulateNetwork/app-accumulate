662bc5a6ac17ab799b162790dc168002960bd3aa
Dennis B <dennis@factoid.org>
2022-09-14 14:04:00 -0500

    wip

---
+++ src/common/keccak.c
@@ -0,0 +1,36 @@
+#include <common/keccak.h>
+#ifndef UNIT_TEST
+#include "os.h"
+#include "cx.h"
+#else
+#include <common/internal/testing/sha3.h>
+#include <string.h>
+#endif
+
+Error keccak(const uint8_t *in, unsigned int len,  uint8_t *out, unsigned int out_len) {
+    if ( !in || !out ) {
+        return ErrorCode(ErrorParameterNil);
+    }
+    if ( out_len != 32 ) {
+        return ErrorCode(ErrorParameterInsufficientData);
+    }
+#ifndef UNIT_TEST
+   int ret = cx_hash_sha256(in, len, out, out_len);
+   if ( ret != 0 ) {
+	   return ErrorCode(ErrorInvalidHashParameters);
+   }
+   cx_sha3_t keccak256;
+   cx_keccak_init(&keccak256, 256);
+   cx_hash((cx_hash_t *) &keccak256, CX_LAST, public_key, 64, address, sizeof(address));
+
+#else
+    sha3_context hash256;
+    sha3_Init256(&hash256);
+    sha3_SetFlags(&hash256, SHA3_FLAGS_KECCAK);
+    sha3_Update(&hash256, in, len);
+    sha3_Finalize(&hash256);
+    memmove(out, hash256.u.sb, out_len);
+#endif
+    return ErrorCode(ErrorNone);
+}
+
