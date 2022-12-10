a3be704cc535fb49dfcbae9b60aeb656fdb4e35f
Dennis B <dennis@factoid.org>
2022-12-10 08:58:28 -0600

    github ci

--- ci/build-gitlab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt update && apt install zip wget -y
-. ${0%/*}/build.sh
-. ${0%/*}/upload.sh
--- ci/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /app
-
-echo uploading assets to package to "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/"
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "dist/$OUTPUT_PKG_NAME.elf" "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/${OUTPUT_PKG_NAME}.elf"
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "dist/$OUTPUT_PKG_NAME.sha256" "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/${OUTPUT_PKG_NAME}.sha256"
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "dist/$OUTPUT_PKG_NAME.hex" "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/${OUTPUT_PKG_NAME}.hex"
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "dist/$OUTPUT_PKG_NAME.apdu" "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/${OUTPUT_PKG_NAME}.apdu"
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "dist/output-scan-build.zip" "${PACKAGE_REGISTRY_URL}/${APP_VERSION}/output-scan-build.zip"
