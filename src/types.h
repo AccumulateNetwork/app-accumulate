f5d24c8e39c42cfc0e94d7820040983cd95fa1b4
Dennis B <dennis@factoid.org>
2022-12-11 08:57:48 -0600

    spelling

--- src/types.h
+++ src/types.h
@@ -111,5 +111,5 @@ typedef struct {
     };
     request_type_e req_type;              /// user request
     uint32_t bip32_path[MAX_BIP32_PATH];  /// BIP32 path
-    uint8_t bip32_path_len;               /// lenght of BIP32 path
+    uint8_t bip32_path_len;               /// length of BIP32 path
 } global_ctx_t;
