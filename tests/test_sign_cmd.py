e1be914a6a9cf9956cb45f5229ffec3ba2808e36
Dennis B <dennis@factoid.org>
2023-05-03 00:57:04 -0500

    fix tests

--- tests/test_sign_cmd.py
+++ tests/test_sign_cmd.py
@@ -21,17 +21,17 @@ def test_sign_tx(cmd, button):
     addCreditsExpectedSig = bytes.fromhex("50edaa8520374aa560845d797a35309caadd823427f4218c1ede364445b884d19671ab5546ab026c388887ec2986ef95e7eef71239bf1c5fc7e2eb040390dc0f")
 
     #signing test vector for an AddCredits transaction
-    #v, sig = cmd.sign_tx(bip32_path=bip32_path,
-    #                         envelope=addCreditsEnv,
-    #                         button=button)
+    v, sig = cmd.sign_tx(bip32_path=bip32_path,
+                             envelope=addCreditsEnv,
+                             button=button)
 
-    #assert sig == addCreditsExpectedSig
+    assert sig == addCreditsExpectedSig
 
 
     sendTokensEnv = base64.b64decode("AWIBAgIg5V2XO/aROByUYCNU0eH2VfexxL1Wdg3/7/or70VB7BEEE2FjYzovL2FkaS5hY21lL0FDTUUFAQbShdjMBAinZDJnLNeDTrJoc6M9PgCjUMWpo6/5t8xQY320kx5LRwNYATYBE2FjYzovL2FkaS5hY21lL0FDTUUChOAy+6ilRW9jHIIqKyRmwYs/p4BDMKuHCI7W4w1pBQUCHgEDBBoBFWFjYzovL290aGVyLmFjbWUvQUNNRQIBZA==")
     sendTokensExpectedSig = bytes.fromhex("d8debe3221731ec4c680edcf38caf08a29e94157f92e5d769bfc942f2f794bd39d13bcbd8ff63d24e060a61a6f0de80b035bc4f52c826104eebf91daefb37309")
 
-    #signing test vector for an AddCredits transaction
+    #signing test vector for an SendTokens transaction
     v, sig = cmd.sign_tx(bip32_path=bip32_path,
                          envelope=sendTokensEnv,
                          button=button)
